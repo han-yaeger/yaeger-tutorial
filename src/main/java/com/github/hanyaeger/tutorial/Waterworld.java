96cc37a1b02ca02405d8b20f028782f7628f5509
Herman Telman <herman.telman@han.nl>
2020-08-25 14:27:29 +0200

    Adding waterworld class

---
+++ src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
@@ -0,0 +1,14 @@
+package com.github.hanyaeger.tutorial;
+import com.github.hanyaeger.api.engine.YaegerGame;
+
+public class Waterworld extends YaegerGame {
+    @Override
+    protected void setupGame() {
+
+    }
+
+    @Override
+    protected void setupScenes() {
+
+    }
+}
