d41d4e1252e88a325db0b46a9aae455b84507ba3
MukhtarAssadi <mukhtarassadi@outlook.com>
2023-03-17 14:02:10 +0100

    Added setup game

--- src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
+++ src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
@@ -2,9 +2,15 @@ package com.github.hanyaeger.tutorial;
 
 import com.github.hanyaeger.api.YaegerGame;
 
+
 public class Waterworld extends YaegerGame {
     public static void main(String[] args){
         launch(args);
+    }
+    @Override
+    protected void setupGame() {
+        setGameTitle("Waterworld");
+        setSize(new Size(800, 600));
     }
 }
 
