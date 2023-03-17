793af486a1165060f0e6b904cfc95ab8cf8c2a12
MukhtarAssadi <mukhtarassadi@outlook.com>
2023-03-17 13:58:00 +0100

    Create Waterworld.java

---
+++ src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
@@ -0,0 +1,10 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Waterworld extends YaegerGame {
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+
