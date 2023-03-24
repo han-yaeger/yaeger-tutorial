31cd70edde0604081c5aa6a4b3b2c20ae5bae2da
roos1044 <ri.lodewijk@student.han.nl>
2023-03-24 14:02:42 +0100

    .

---
+++ src/main/java/com/github/hanyaeger/tutorial/platforms/Platform.java
@@ -0,0 +1,15 @@
+package com.github.hanyaeger.tutorial.platforms;
+
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.impl.SpriteEntity;
+import com.github.hanyaeger.tutorial.Player;
+
+public class Platform extends SpriteEntity implements Collider {
+    Platform(Player player){
+
+    }
+
+    public boolean staatErop(){
+
+    }
+}
---
+++ src/main/java/com/github/hanyaeger/tutorial/platforms/VastPlatform.java
@@ -0,0 +1,15 @@
+package com.github.hanyaeger.tutorial.platforms;
+
+import com.github.hanyaeger.tutorial.Player;
+
+public class VastPlatform extends Platform{
+
+    VastPlatform(Player player) {
+        super(player);
+    }
+
+    @Override
+    public boolean staatErop(){
+
+    }
+}
