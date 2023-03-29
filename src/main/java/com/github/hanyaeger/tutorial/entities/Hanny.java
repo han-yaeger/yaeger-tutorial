8dc5e87bc394981370f5607cc2a698ba6479e6aa
MukhtarAssadi <mukhtarassadi@outlook.com>
2023-03-29 08:13:39 +0200

    tutorial verder gemaakt

---
+++ src/main/java/com/github/hanyaeger/tutorial/entities/Hanny.java
@@ -0,0 +1,57 @@
+package com.github.hanyaeger.tutorial.entities;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.Newtonian;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
+
+import java.util.Set;
+
+
+public class Hanny extends DynamicSpriteEntity implements KeyListener, SceneBorderTouchingWatcher, Newtonian {
+    public Hanny(Coordinate2D location){
+        super("sprites/hanny.png", location, new Size(20,40), 1, 2);
+        setGravityConstant(0.005);
+        setFrictionConstant(0.04);
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        if(pressedKeys.contains(KeyCode.LEFT)){
+            setCurrentFrameIndex(0);
+            setMotion(3,270d);
+        } else if(pressedKeys.contains(KeyCode.RIGHT)){
+            setCurrentFrameIndex(1);
+            setMotion(3,90d);
+        } else if(pressedKeys.contains(KeyCode.UP)){
+            setMotion(3,180d);
+        } else if(pressedKeys.contains(KeyCode.DOWN)){
+            setMotion(3,0d);
+        }
+    }
+
+    @Override
+    public void notifyBoundaryTouching(SceneBorder border) {
+        setSpeed(0);
+
+        switch(border){
+            case TOP:
+                setAnchorLocationY(1);
+                break;
+            case BOTTOM:
+                setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+                break;
+            case LEFT:
+                setAnchorLocationX(1);
+                break;
+            case RIGHT:
+                setAnchorLocationX(getSceneWidth() - getWidth() - 1);
+            default:
+                break;
+        }
+    }
+}
--- src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
@@ -2,6 +2,7 @@ package com.github.hanyaeger.tutorial.scenes;
 
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.tutorial.entities.Hanny;
 import com.github.hanyaeger.tutorial.entities.Swordfish;
 
 public class GameLevel extends DynamicScene {
@@ -15,5 +16,7 @@ public class GameLevel extends DynamicScene {
     public void setupEntities() {
         var swordfish = new Swordfish(new Coordinate2D(getHeight() / 2, getWidth() / 2));
         addEntity(swordfish);
+        var hanny = new Hanny(new Coordinate2D(getHeight() / 10, getWidth() / 10));
+        addEntity(hanny);
     }
 }
